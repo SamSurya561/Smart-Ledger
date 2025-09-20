f4c8eb0f6c75518613b699515e0dea7cb9267388
samsurya561 <samsurya561@gmail.com>
2025-09-20 08:14:06 +0000

    Update Android build configuration

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.surya.smartledger"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.surya.smartledger"
         minSdk = flutter.minSdkVersion
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
     }
